88da061e319c9d8c6d54ead554c27e7f3ceddcd3
meteopavel <isu@meteopavel.ru>
2026-06-13 19:05:53 +0800

    Fix skeleton preloaders: stagger animation phases per element

--- source/scripts_generator/scripts/loading.js
+++ source/scripts_generator/scripts/loading.js
@@ -2,19 +2,35 @@ function setupImgSkeleton(img) {
   if (img.complete && img.naturalWidth > 0) {
     img.classList.add('img-loaded');
   } else {
-    img.addEventListener('load', function () {
-      img.classList.add('img-loaded');
-    });
-    img.addEventListener('error', function () {
-      img.classList.add('img-loaded');
-    });
+    img.addEventListener('load', function () { img.classList.add('img-loaded'); });
+    img.addEventListener('error', function () { img.classList.add('img-loaded'); });
   }
 }
 
-document
-  .querySelectorAll('.main-info__photo, .project__shields img, .video-preview__image')
-  .forEach(setupImgSkeleton);
+function setupSvgSkeleton(svg) {
+  var useEl = svg.querySelector('use');
+  if (!useEl) { svg.classList.add('svg-loaded'); return; }
+  var href = useEl.getAttribute('href') || useEl.getAttribute('xlink:href') || '';
+  var filePath = href.split('#')[0];
+  if (!filePath) { svg.classList.add('svg-loaded'); return; }
+  fetch(filePath)
+    .then(function () { svg.classList.add('svg-loaded'); })
+    .catch(function () { svg.classList.add('svg-loaded'); });
+}
 
-window.addEventListener('load', function () {
-  document.body.classList.remove('loading');
+document.querySelectorAll('.main-info__photo, .video-preview__image').forEach(setupImgSkeleton);
+
+document.querySelectorAll('.project__shields img').forEach(function (img, i) {
+  img.style.animationDelay = '-' + ((i * 0.25) % 1.5) + 's';
+  setupImgSkeleton(img);
+});
+
+document.querySelectorAll('svg.contact-icon').forEach(function (svg, i) {
+  svg.style.animationDelay = '-' + ((i * 0.2) % 1.5) + 's';
+  setupSvgSkeleton(svg);
+});
+
+document.querySelectorAll('svg.preview-icon').forEach(function (svg, i) {
+  svg.style.animationDelay = '-' + ((i * 0.3) % 1.5) + 's';
+  setupSvgSkeleton(svg);
 });
